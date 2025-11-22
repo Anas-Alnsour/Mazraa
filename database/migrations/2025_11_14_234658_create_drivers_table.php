4466fe718b338a1bc0494897272551296e6cf1e0
Adnan204awad <160602050+Adnan204awad@users.noreply.github.com>
2025-11-22 21:20:04 +0300

    Update transport_type enum values with capacity

--- database/migrations/2025_11_14_234658_create_drivers_table.php
+++ database/migrations/2025_11_14_234658_create_drivers_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
         Schema::create('drivers', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->enum('transport_type', ['Car', 'Bus', 'Large Bus']);
+            $table->enum('transport_type', ['Car (Max 4 passengers)', 'Bus (Max 20 passengers)', 'Large Bus (Max 50 passengers)']);
             $table->timestamps();
         });
     }
